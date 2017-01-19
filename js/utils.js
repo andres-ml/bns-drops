0b45ae910b23528110e449eaad16a67798e24cdc
Andres <andres@menuterraneus.com>
2017-01-19 11:18:57 +0100

    style + cleanup

--- js/utils.js
+++ js/utils.js
@@ -1,28 +1,22 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * 
  */
 
-
-$.fn.outerHtml = function() {
-    return $('<div>').append($(this).clone()).html();
-};
-
-$.fn.toggleEased = function(speed) {
-    var visible = $(this).is(':visible');
+(function($, window) {
     
-    if (!visible) {
-        $(this).slideDown(speed);
-    }
-    else {
-        $(this).slideUp(speed);
-    }
-};
-
-$(document.body).on('keyup', 'input', function(event) {
-    var target = $(this).data('bind-enter');
-    if (target && event.keyCode === 13) {
-        $('#' + target).click();
-    }
-});
+    /**
+     * Allows linking of inputs to buttons.
+     * 
+     * Simply add data-bind-enter="button-id" to your input element
+     * and the specified button will be clicked whenever 'enter' key is pressed
+     * in your input.
+     * 
+     */
+    $(window.document.body).on('keyup', 'input', function(event) {
+        var target = $(this).data('bind-enter');
+        if (target && event.keyCode === 13) {
+            $('#' + target).click();
+        }
+    });
+    
+})($, window);
